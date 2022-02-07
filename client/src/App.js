5adb075b376f1b3be019521438650e3ca38f9c0d
Georgi Petrov <georgipeetrov@gmail.com>
2022-02-07 13:54:00 +0200

    added getHeaderData function

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WorkExpierence from './components/WorkExpierence';
 import RegularList from './components/RegularList';
 import Education from './components/Education';
@@ -7,64 +7,24 @@ import { ResumeView } from './components/ResumeView';
 import axios from 'axios';
 // import { ResumeMain } from './components/ResumeMain';
 
-const workExpierence = [
-  {
-    subheader: "2014 - 2019, Kruisender, Etten-Lour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-  {
-    subheader: "2015 - 2017, Kruisender, Etten-Lour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-  {
-    subheader: "2017 - 2019, Kruisender, Etten-Lour",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-]
-
-const education = [
-  {
-    subheader: "HBO - Cretive Business",
-    years: "2005 - 2015",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-  {
-    subheader: "Media Graphic Designer",
-    years: "2005 - 2015",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-  {
-    subheader: "Something else",
-    years: "2005 - 2015",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente aliquid minima, sint ratione sunt et at voluptate repudiandae ea sit sequi odit accusantium consequuntur. Est amet quisquam dolorum esse harum!"
-  },
-];
-
-const headerInfo = 
-  {
-    avatar: "https://www.thesprucepets.com/thmb/sfuyyLvyUx636_Oq3Fw5_mt-PIc=/3760x2820/smart/filters:no_upscale()/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
-    name: "EMMA WILLIAMS",
-    title: "GRAPHIC DESIGNER",
-    bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat suscipit, doloribus magnam odio totam aperiam sunt expedita nesciunt! Adipisci corrupti ducimus voluptatibus exercitationem, praesentium architecto perspiciatis ratione distinctio deleniti minus.",
-    contactInfo: [
-      {email: "emial@email.com"},
-      {phone: "08888888"},
-      {website: "website.com"},
-      {linkedIn: "Emma Williams"},
-    ]
+function App() {
+  const [headerData, setHeaderData] = useState({});
+  
+  const getHeaderData = (data, getter, setter) => {
+    setter(data);
+    return getter;
   }
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/headerInfo')
+    .then(function (response) {
+      getHeaderData(response.data, headerData, setHeaderData);
+    })
+  }, []);
 
-
-function App() {
-  axios.get('http://localhost:3000/posts')
-  .then(function (response) {
-    console.log(response);
-  })
   return (
     <>
-      <ResumeHeader info={headerInfo} />
-      {/* <ResumeMain info={} /> */}
+      <ResumeHeader info={headerData} />
     </>
   );
 }
